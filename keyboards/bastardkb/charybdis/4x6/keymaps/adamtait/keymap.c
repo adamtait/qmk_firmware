6ef8557875f0a739d112a39f1a55238cb39f3d62
adamtait <bin@adamta.it>
2022-03-07 13:31:00 -0800

    move around thumb clusters, arrow keys

--- keyboards/bastardkb/charybdis/4x6/keymaps/adamtait/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/adamtait/keymap.c
@@ -41,23 +41,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LSFT,    KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,       KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_RSFT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   KC_LAPO, KC_LCTL, KC_LGUI,     RAISE,  KC_ENT,
-                                            MOUSE,   KC_BSPC,     KC_SPC
+                                  KC_BSPC, KC_LGUI, KC_LAPO,     KC_ENT,  KC_SPC,
+                                           KC_LCTL,   MOUSE,      RAISE
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
   
   [LAYER_MOUSE] = LAYOUT_charybdis_4x6(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-      KC_TILD,  KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+       _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       _______, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    XXXXXXX, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_VOLU,
+       _______, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    XXXXXXX, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, _______,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       _______, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, KC_BTN1,    KC_BTN1, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, KC_MUTE,
+       _______, XXXXXXX, KC_BTN2, DRGSCRL, KC_BTN1, KC_LGUI,    KC_BTN1, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, _______,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       _______, KC_LGUI, KC_LALT, KC_LCTL, DRGSCRL, KC_BTN2,    KC_BTN2, DRGSCRL, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
+       _______, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, KC_BTN2,    KC_BTN2, DRGSCRL, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  _______, _______, _______,    XXXXXXX, _______,
-                                           XXXXXXX, _______,    _______
+                                  _______, _______, _______,    _______,  _______,
+                                           _______, XXXXXXX,    XXXXXXX
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -65,14 +65,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────-────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
         KC_F12,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   // ├───────────-───────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_MOD,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LBRC,   KC_P7,   KC_P8,   KC_P9, KC_RBRC, XXXXXXX,
+       RGB_MOD,  _______, _______, _______, _______, XXXXXXX,    XXXXXXX, _______, _______, _______, _______, KC_VOLU,
   // ├────────────-──────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_TOG,  KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_BTN1,    KC_PPLS,   KC_P4,   KC_P5,   KC_P6, KC_PMNS, KC_PEQL,
+       RGB_TOG,  _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, KC_MUTE,
   // ├─────────────-─────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, EEP_RST, KC_BTN2,    KC_PAST,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_PDOT,
+       RGB_RMOD, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, KC_VOLD,
   // ╰──────────────-────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  _______, _______, _______,    XXXXXXX, _______,
-                                           XXXXXXX,  KC_DEL,      KC_P0
+                                   KC_DEL, _______, _______,    _______, _______,
+                                           _______, XXXXXXX,    XXXXXXX
   //                            ╰────-───────────────────────╯ ╰──────────────────╯
   ),
 };
