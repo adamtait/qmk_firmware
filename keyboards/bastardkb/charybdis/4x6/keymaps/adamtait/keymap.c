8b13ba9815c460712f81effaf023f5d75a5fce6e
adamtait <bin@adamta.it>
2022-04-06 23:00:22 -0700

    improvement on modifier keys and 2nd layer (hopefully)

--- keyboards/bastardkb/charybdis/4x6/keymaps/adamtait/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/adamtait/keymap.c
@@ -34,17 +34,26 @@
 //
 // KEY CONFIGURATION
 
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
 
 enum charybdis_keymap_layers {
-    LAYER_BASE = 0,
+    LAYER_DVORAK = 0,
     LAYER_NAV,
-    LAYER_EMACS,
-    LAYER_SYS,
+    //    LAYER_EMACS,
+    //    LAYER_SYS,
 };
 
+#define DVORAK TT(LAYER_DVORAK)
 #define NAV TT(LAYER_NAV)
-#define SYS TT(LAYER_SYS)
-#define EMACS TT(LAYER_SYS)
+//#define SYS TT(LAYER_SYS)
+//#define EMACS TT(LAYER_EMACS)
+
+#define TAB_LFT LSG(KC_LBRC)
+#define TAB_RGT LSG(KC_RBRC)
+#define CMD_LFT LGUI(KC_LEFT)
+#define CMD_RGT LGUI(KC_RIGHT)
+#define CMD_TLD LGUI(KC_TILD)
 
 
 
@@ -52,67 +61,64 @@ enum charybdis_keymap_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // DVORAK
-  [LAYER_BASE] = LAYOUT_charybdis_4x6(
+  [LAYER_DVORAK] = LAYOUT_charybdis_4x6(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       KC_EQUAL,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_GESC,
+       KC_GESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_EQUAL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_TAB,     KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,       KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_SLSH,
+       KC_TAB,    KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,       KC_F,    KC_G,    KC_C,    KC_R,    KC_L,  KC_SLSH,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MINUS,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,       KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_BSLS,
+       KC_MINUS,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,       KC_D,    KC_H,    KC_T,    KC_N,    KC_S,  KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LSFT,    KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,       KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_RSFT,
+       KC_LSFT,   KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,       KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,  KC_RSFT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_BSPC, KC_LGUI, KC_LAPO,     KC_ENT,  RALT_T(KC_SPC),
-                                           KC_LCTL,   NAV,        EMACS
+                                     KC_BSPC, NAV, KC_LCTL,      KC_ENT, KC_SPC,
+                                          KC_LGUI, KC_LAPO,     KC_LGUI
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
-
-  // KC_HOME, KC_PGUP, KC_PGDN,  KC_END
-  
   [LAYER_NAV] = LAYOUT_charybdis_4x6(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______,
+       CMD_TLD,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       _______, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, _______,    _______, LSG(KC_LBRC), LGUI(KC_LEFT), LGUI(KC_RIGHT), LSG(KC_RBRC), _______, 
+       _______, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_HOME,    KC_PGUP, TAB_LFT, CMD_LFT, CMD_RGT, TAB_RGT, _______, 
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       _______, _______, KC_BTN2, DRGSCRL, KC_BTN1, _______,    _______, KC_BTN1, DRGSCRL, KC_BTN2, _______, _______,
+       _______, _______, KC_BTN2, DRGSCRL, KC_BTN1,  KC_END,    KC_PGDN, KC_BTN1, DRGSCRL, KC_BTN2, _______, _______,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        _______, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, _______,    _______, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, _______,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  _______, _______, _______,    _______,  _______,
-                                           _______, XXXXXXX,    XXXXXXX
+                                  _______,  DVORAK, _______,    _______,  _______,
+                                           _______, _______,    _______
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
-  [LAYER_EMACS] = LAYOUT_charybdis_4x6(
+  //[LAYER_EMACS] = LAYOUT_charybdis_4x6(
   // ╭──────────-────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-        KC_F12,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+  //      KC_F12,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   // ├───────────-───────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_MOD,  _______, _______, _______, _______, XXXXXXX,    XXXXXXX, _______, _______, _______, _______, KC_VOLU,
+  //     RGB_MOD,  _______, _______, _______, _______, XXXXXXX,    XXXXXXX, _______, _______, _______, _______, KC_VOLU,
   // ├────────────-──────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_TOG,  _______, _______, KC_LBRC, KC_LPRN, _______,    _______, KC_RPRN, KC_RBRC, _______, _______, KC_MUTE,
+  //     RGB_TOG,  _______, _______, KC_LBRC, KC_LPRN, _______,    _______, KC_RPRN, KC_RBRC, _______, _______, KC_MUTE,
   // ├─────────────-─────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_RMOD, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, KC_VOLD,
+  //     RGB_RMOD, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, KC_VOLD,
   // ╰──────────────-────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   KC_DEL, _______, _______,    _______, _______,
-                                           _______, XXXXXXX,    XXXXXXX
+  //                                 KC_DEL,      NAV, _______,        DVORAK, _______,
+  //                                          _______, _______,    XXXXXXX
   //                            ╰────-───────────────────────╯ ╰──────────────────╯
-  ),
+  //),
 
-  [LAYER_SYS] = LAYOUT_charybdis_4x6(
+  //[LAYER_SYS] = LAYOUT_charybdis_4x6(
   // ╭──────────-────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-        KC_F12,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+  //      KC_F12,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   // ├───────────-───────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_MOD,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
+  //     RGB_MOD,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
   // ├────────────-──────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_TOG,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE,
+  //     RGB_TOG,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE,
   // ├─────────────-─────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
+  //     RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
   // ╰──────────────-────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,
-                                           XXXXXXX, XXXXXXX,    XXXXXXX
+  //                                 XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,
+  //                                         XXXXXXX, XXXXXXX,    XXXXXXX
   //                            ╰────-───────────────────────╯ ╰──────────────────╯
-  ),
+  //),
 };
 // clang-format on
