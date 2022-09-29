94114a553c38cb8cd2b35789824b6437e9765115
adamtait <bin@adamta.it>
2022-09-29 09:15:30 -0700

    build & flash scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+qmk compile -kb bastardkb/charybdis/4x6 -km adamtait
---
+++ flash_right.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+qmk flash -kb bastardkb/charybdis/4x6 -km adamtait -bl dfu-split-right
